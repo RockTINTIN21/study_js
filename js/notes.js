78d61871004e53741b9447c7011bd1b655d84799
RockTINTIN21 <sashalexjr@gmail.com>
2023-10-10 20:24:57 +0500

    1 commit

---
+++ js/notes.js
@@ -0,0 +1,46 @@
+"use strict";
+
+
+// let number = 5;
+// const leftBorderWidth = 1;
+//
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     name:"John",
+//     age:25
+// };
+// console.log(obj["name"]);
+//
+// let arr = ['test.png', 'orange','blyat'];
+// console.log(arr[0])
+
+
+
+// alert('hello');
+
+// const result = confirm("Ты тут?");
+// console.log(result)
+
+// const answer = prompt("Вам есть 18?","18");
+// console.log(typeof(answer));
+
+// const answers = [];
+// answers[0]= prompt('Как вас зовут?','');
+// answers[1]= prompt('Как ваша фамилия?','');
+// answers[2]= prompt('Сколько вам лет','');
+//
+// document.write(answers);
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+//
+// const user = "Ivan";
+// alert(`Привет ${user}`)
+
+// let incr = 10;
+// incr++;
+// console.log(incr);
